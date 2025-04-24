c620b288dc11660838f8386ca5fd2dfafaca68dd
ByteWizard2 <shaheershahir7022@gmail.com>
2025-04-24 15:01:23 +0530

    create the all  pages UI

--- frontend-react-ts/src/pages/dashboard/OwnerPage.tsx
+++ frontend-react-ts/src/pages/dashboard/OwnerPage.tsx
@@ -1,10 +1,12 @@
+import PageAccessTemplate from '../../components/dashboard/page-access/PageAccessTemplate';
+import { FaUserCog } from 'react-icons/fa';
 
 const OwnerPage = () => {
   return (
-    <div>
-      
+    <div className='pageTemplate2'>
+      <PageAccessTemplate color='#3b3549' icon={FaUserCog} role='Owner' />
     </div>
-  )
-}
+  );
+};
 
-export default OwnerPage
+export default OwnerPage;
